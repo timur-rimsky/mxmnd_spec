314bfd0a2db8a6a606e7b9e119c341853ce3d9ea
Timur <timur.rimsky@gmail.com>
2018-10-12 12:11:15 +0300

    added mxmnd_spec

---
+++ spec/tmp/yan.js
@@ -0,0 +1,6 @@
+{
+   "X-Cloud-Trace-Context": "3842e387034da861559c0f117f472663/9518555800889215770",
+   "Host": "headers.jsontest.com",
+   "User-Agent": "curl/7.58.0",
+   "Accept": "*/*"
+}
---
+++ spec/tmp/yan1.js
@@ -0,0 +1,14 @@
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+Content-Type: application/json; charset=ISO-8859-1
+X-Cloud-Trace-Context: 3842e387034da861559c0f117f472663
+Date: Wed, 10 Oct 2018 14:20:11 GMT
+Server: Google Frontend
+Content-Length: 174
+
+{
+   "X-Cloud-Trace-Context": "3842e387034da861559c0f117f472663/9518555800889215770",
+   "Host": "headers.jsontest.com",
+   "User-Agent": "curl/7.58.0",
+   "Accept": "*/*"
+}
